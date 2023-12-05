eee0c5ef44a05d906fe0bb219b9e2d3e6ce75f17
Tekle <119097475+Teklez@users.noreply.github.com>
2023-12-05 19:17:00 +0300

    Added solution for Watering Plants

---
+++ python-track/watering-plants.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        steps = 0
+        cap = capacity
+        for i in range(len(plants)):
+            if plants[i] > capacity:
+                capacity = cap
+                steps += 2*i
+            steps += 1
+            capacity -= plants[i]
+        return steps
+
+                
