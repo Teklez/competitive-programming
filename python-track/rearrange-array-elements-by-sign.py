14c8942118e569d8f933d60b99a3be7c0f719eaf
Tekle <119097475+Teklez@users.noreply.github.com>
2023-12-07 09:45:29 +0300

    Added solution for Rearrange Array Elements by Sign

---
+++ python-track/rearrange-array-elements-by-sign.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        res = [0]*len(nums)
+        i = 0
+        j = 1
+        for k in range(len(nums)):
+            if nums[k] > 0:
+                res[i] = nums[k]
+                i += 2
+            else:
+                res[j] = nums[k]
+                j += 2
+        return res
