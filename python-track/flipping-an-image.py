a0a8f98043ed85fb259ba3586236b282e7e27ac8
Tekle <119097475+Teklez@users.noreply.github.com>
2023-12-22 17:23:13 +0300

    Added solution for Flipping an Image

--- python-track/flipping-an-image.py
+++ python-track/flipping-an-image.py
@@ -1,20 +1,4 @@
-class Solution(object):
-    def flipAndInvertImage(self, image):
-        """
-        :type image: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        for row in image:
-            i = 0
-            j = len(row) - 1
-            while i < j:
-                row[i], row[j] = row[j], row[i]
-                i += 1
-                j -= 1
-            for i in range(len(row)):
-                if row[i] == 1:
-                    row[i] = 0
-                else:
-                    row[i] = 1
-        return image
-                
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        return [[1^i for i in reversed(row )] for row in image]
+        
