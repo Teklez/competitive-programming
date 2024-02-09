70ec07090226d2e08e422645c57563cdbdf0d6ed
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-02-09 10:45:07 +0300

    Add solution for Sum of Absolute Differences in a Sorted Array

--- a2sv/folder/leetcode/sum-of-absolute-differences-in-a-sorted-array.py
+++ a2sv/folder/leetcode/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,17 +1,18 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        preSum = [0]*len(nums)
-        postSum = [0]*len(nums)
-        sumPre = 0
-        sumPost = 0
-        for i in range(len(nums)):
-            preSum[i] = sumPre
-            postSum[len(nums) - i -1] = sumPost
-            sumPre += nums[i]
-            sumPost += nums[len(nums) - i - 1]
+        # preSum = [0]*len(nums)
+        # postSum = [0]*len(nums)
+        # sumPre = 0
+        # sumPost = 0
+        # for i in range(len(nums)):
+        #     preSum[i] = sumPre
+        #     postSum[len(nums) - i -1] = sumPost
+        #     sumPre += nums[i]
+        #     sumPost += nums[len(nums) - i - 1]
         
-        res = []
+        # res = []
+        tot, preSum = sum(nums), 0
         for i in range(len(nums)):
-            absolute = nums[i]*(i) - preSum[i] + postSum[i] - nums[i]*(len(nums) - i - 1)
-            res.append(absolute)
-        return res
+            preSum += nums[i]
+            nums[i] = nums[i]*(i + 1) - preSum + (tot - preSum) - nums[i]*(len(nums) - i - 1)
+        return nums
