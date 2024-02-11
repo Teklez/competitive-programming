5a9efc8b29f65e8fabf0daa2bb680f569768576a
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-02-11 23:12:27 +0300

    Add solution for Sum of Distances

---
+++ a2sv/folder/leetcode/sum-of-distances.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        equalIndexContainer = defaultdict(list)
+        for i in range(len(nums)):
+            equalIndexContainer[nums[i]].append(i)
+        
+        print(equalIndexContainer)
+        for num , indexList in equalIndexContainer.items():
+            tot = sum(indexList)
+            preSum = 0
+            for k in range(len(indexList)):
+                preSum += indexList[k]
+                distance = indexList[k]*(k + 1) - preSum + (tot - preSum) - indexList[k]*(len(indexList) - k - 1)
+                nums[indexList[k]] = distance
+               
+        return nums
+
