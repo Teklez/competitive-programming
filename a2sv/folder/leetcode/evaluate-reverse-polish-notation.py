a8a1de7dec7550e2ca19444eb572ca3b550bfce4
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-02-19 10:24:45 +0300

    Add solution for Evaluate Reverse Polish Notation

---
+++ a2sv/folder/leetcode/evaluate-reverse-polish-notation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        def evaluate(operand1, operand2, operator):
+            result = 0
+            if operator == "+":
+                result = operand1 + operand2
+            elif operator == "-":
+                result = operand1 - operand2
+            elif operator == "*":
+                result = operand1 * operand2
+            else:
+                result = int(operand1 / operand2)
+            return result
+        expression = []
+        operators = {"*", "-", "+", "/"}
+        result = 0
+        for token in tokens:
+            if token in operators:
+                operand2 = expression.pop()
+                operand1  = expression.pop()
+                expression.append(evaluate(operand1, operand2, token))
+            else:
+                expression.append(int(token))
+        return expression[-1]
+
+
+
