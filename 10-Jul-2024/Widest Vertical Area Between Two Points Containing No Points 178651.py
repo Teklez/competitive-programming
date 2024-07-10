aead92bc34859750105e60c049697e0e90486d31
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-07-10 22:07:55 +0300

    Created 'Widest Vertical Area Between Two Points Containing No Points' by A2SV Hub

---
+++ 10-Jul-2024/Widest Vertical Area Between Two Points Containing No Points 178651.py
@@ -0,0 +1,16 @@
+# Problem: Widest Vertical Area Between Two Points Containing No Points - https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x:x[0]) 
+        
+        maxm = 0
+        for i in range(len(points) - 1):
+            maxm = max(maxm, points[i + 1][0] - points[i][0])
+        return maxm
+        
+        
+
+
+
+
