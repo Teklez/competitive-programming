7f14d8f0cf75ff862fbfd919b3f5818525847e0f
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-07-20 16:13:30 +0300

    Created 'Corporate Flight Bookings' by A2SV Hub

---
+++ 20-Jul-2024/Corporate Flight Bookings 182129.py
@@ -0,0 +1,19 @@
+# Problem: Corporate Flight Bookings - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        res = [0]*(n)
+        for i, j , k in bookings:
+            res[i - 1] += k
+            if j  < n:
+                res[j] -= k
+
+        summ = 0
+        for j in range(n):
+            summ += res[j]
+            res[j] = summ
+        return res
+
+        
+
+
