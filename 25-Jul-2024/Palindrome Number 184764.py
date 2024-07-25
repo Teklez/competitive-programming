142fe4b4aeb255c8f4dcc7ab3b0e73008ed02187
Zemenu Mekuria <119097475+Teklez@users.noreply.github.com>
2024-07-25 22:18:28 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 25-Jul-2024/Palindrome Number 184764.py
@@ -0,0 +1,18 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp = x
+        reversed = 0
+        while x > 0:
+            reversed = reversed * 10 + x % 10
+            x = x // 10
+        return temp == reversed
+    
+
+        
+        
